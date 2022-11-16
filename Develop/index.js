1cfe4328947508df580152bc1fb37c33d7961e91
elReclusa <howell_j@hotmail.com>
2022-11-16 15:21:08 -0600

    Added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,58 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your GitHub Username?',
+            name: 'name',
+        }
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?',
+        },
+        {
+            type: 'input',
+            message: 'What is your project description?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions.',
+            name: 'installationInstructions',
 
+        },
+        {
+            type: 'input',
+            message: 'Enter usage information here:',
+            name: 'usageInfo',
+
+        },
+        {
+            type: 'input',
+            message: 'Enter contribution guidelines:',
+            name: 'contributionGuidelines',
+
+        },
+        {
+            type: 'input',
+            message: 'Enter Test instructions:',
+            name: 'test',
+        }
+        {
+            type: 'input',
+            message: 'Enter contact info for questions:',
+            name: 'questions',
+        }
+    ])
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
